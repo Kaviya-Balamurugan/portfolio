632ad11c2043a05dbcb582983f7fafba1e5b811a
Kaviya B <kaviyabalamurugan03@gmai.com>
2026-04-15 14:44:22 +0530

    Added premium animation UI

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,12 +4,12 @@ function Hero() {
   return (
     <section className="hero">
 
-      {/* LEFT CONTENT */}
+      {/* LEFT */}
       <motion.div
         className="hero-text"
-        initial={{ opacity: 0, x: -60 }}
+        initial={{ opacity: 0, x: -40 }}
         animate={{ opacity: 1, x: 0 }}
-        transition={{ duration: 0.7 }}
+        transition={{ duration: 0.6 }}
       >
         <h1>
           Hi, I'm <span>Kaviya</span>
@@ -24,19 +24,18 @@ function Hero() {
 
         <div className="hero-buttons">
           <a href="#projects" className="btn-primary">View Projects</a>
-
           <a href="/resume.pdf" className="btn-secondary" download>
             Download Resume
           </a>
         </div>
       </motion.div>
 
-      {/* RIGHT IMAGE */}
+      {/* RIGHT */}
       <motion.div
         className="hero-image"
-        initial={{ opacity: 0, x: 60 }}
+        initial={{ opacity: 0, x: 40 }}
         animate={{ opacity: 1, x: 0 }}
-        transition={{ duration: 0.7 }}
+        transition={{ duration: 0.6 }}
       >
         <img src={require("../assets/profile.jpg")} alt="profile" />
       </motion.div>
