aab34b75425d5a3b2573404f36fc650ef01660fd
Kaviya B <kaviyabalamurugan03@gmai.com>
2026-04-15 14:05:22 +0530

    Added premium animation UI

--- src/App.js
+++ src/App.js
@@ -1,30 +1,19 @@
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import About from "./components/About";
-import Skills from "./components/Skills";
-import Projects from "./components/Projects";
-import Contact from "./components/Contact";
-import Experience from "./components/Experience";
-import Achievements from "./components/Achievements";
-import WhatIDo from "./components/WhatIDo";
-import Education from "./components/Education";
-import "./App.css";
-
 function App() {
   return (
     <>
       <Navbar />
-      <Hero />
-      <About />
-      <Education />
-      <WhatIDo />
-      <Experience />
-      <Skills />
-      <Projects />
-      <Achievements />
-      <Contact />
+
+      <main>
+        <Hero />
+        <About />
+        <Education />
+        <WhatIDo />
+        <Experience />
+        <Skills />
+        <Projects />
+        <Achievements />
+        <Contact />
+      </main>
     </>
   );
 }
-
-export default App;
