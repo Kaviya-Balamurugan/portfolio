9d0d5357258e4ee6cfe942226af4683d6502068b
Kaviya B <kaviyabalamurugan03@gmai.com>
2026-04-15 14:39:46 +0530

    Added premium animation UI

--- src/App.js
+++ src/App.js
@@ -2,11 +2,8 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Education from "./components/Education";
-import WhatIDo from "./components/WhatIDo";
-import Experience from "./components/Experience";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
-import Achievements from "./components/Achievements";
 import Contact from "./components/Contact";
 import "./App.css";
 
@@ -19,11 +16,8 @@ function App() {
         <Hero />
         <About />
         <Education />
-        <WhatIDo />
-        <Experience />
         <Skills />
         <Projects />
-        <Achievements />
         <Contact />
       </main>
     </>
