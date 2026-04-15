2e54725201b1774fb87ed1d7df40b47b45eb7fcc
Kaviya B <kaviyabalamurugan03@gmai.com>
2026-04-15 16:36:29 +0530

    Added premium animation UI

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,26 @@
 function Footer() {
   return (
     <footer className="footer">
-      <p>© 2026 Kaviya | AI & ML Engineer</p>
+      <p>© Kaviya Balamurugan | AI & ML Engineer</p>
+
       <div className="footer-links">
+
         <a href="https://github.com/Kaviya-Balamurugan" target="_blank" rel="noopener noreferrer">
           GitHub
         </a>
+
         <a href="https://linkedin.com/in/kaviyabalamurugan" target="_blank" rel="noopener noreferrer">
           LinkedIn
         </a>
+
+        <a href="https://leetcode.com/u/Kaviya_Balamurugan/" target="_blank" rel="noopener noreferrer">
+          LeetCode
+        </a>
+
+        <a href="https://www.hackerrank.com/profile/Kaviya03" target="_blank" rel="noopener noreferrer">
+          HackerRank
+        </a>
+
       </div>
     </footer>
   );
