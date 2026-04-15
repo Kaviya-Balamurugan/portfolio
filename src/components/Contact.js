6b89f4237a79b576c4417390c2ac963d36aa1b1f
Kaviya B <kaviyabalamurugan03@gmai.com>
2026-04-15 11:39:52 +0530

    updated frontend

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,27 @@
 function Contact() {
   return (
-    <section id="contact" className="contact">
+    <section id="contact">
       <h2>Contact</h2>
 
-      <p>Email: kaviyabalamurugan03@gmail.com</p>
-      <p>GitHub: https://github.com/Kaviya-Balamurugan</p>
-      <p>LinkedIn: https://www.linkedin.com/in/kaviyabalamurugan</p>
+      <div className="contact-card">
+
+        <p><b>Email:</b> kaviya.balamurugan03@gmail.com</p>
+
+        <p>
+          <b>GitHub:</b>{" "}
+          <a href="https://github.com/Kaviya-Balamurugan" target="_blank">
+            github.com/Kaviya-Balamurugan
+          </a>
+        </p>
+
+        <p>
+          <b>LinkedIn:</b>{" "}
+          <a href="https://linkedin.com/in/kaviyabalamurugan" target="_blank">
+            linkedin.com/in/kaviyabalamurugan
+          </a>
+        </p>
+
+      </div>
     </section>
   );
 }
