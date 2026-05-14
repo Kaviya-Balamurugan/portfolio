53a0a12647ea629703d51b7b55194908542fe6ae
Kaviya B <kaviyabalamurugan03@gmai.com>
2026-05-14 10:02:38 +0530

    upgrade

--- src/components/Reveal.js
+++ src/components/Reveal.js
@@ -3,9 +3,9 @@ import { motion } from "framer-motion";
 const Reveal = ({ children }) => {
   return (
     <motion.div
-      initial={{ opacity: 0, scale: 0.95, filter: "blur(10px)" }}
-      whileInView={{ opacity: 1, scale: 1, filter: "blur(0px)" }}
-      transition={{ duration: 0.7, ease: "easeOut" }}
+      initial={{ opacity: 0, y: 60 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.7 }}
       viewport={{ once: true }}
     >
       {children}
