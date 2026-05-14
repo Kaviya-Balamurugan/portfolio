1468b26a9ce44bebecdf60f599d0c60120d13237
Kaviya B <kaviyabalamurugan03@gmai.com>
2026-05-14 10:45:03 +0530

    update

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,3 +1,12 @@
+import brainDet from "../assets/brain_det.png";
+import brainLogin from "../assets/brain_login.png";
+import genQuiz from "../assets/gen-quiz.png";
+import genAiTut from "../assets/genai-tut.png";
+import genImg from "../assets/gen.png";
+import tripCost from "../assets/trip_cost.png";
+import tripImg from "../assets/trip.png";
+import tumAnly from "../assets/tum_anly.png";
+import tumDet from "../assets/Tum_det.png";
 function Projects() {
   return (
     <section id="projects">
@@ -7,8 +16,21 @@ function Projects() {
 
         {/* Project 1 */}
         <div className="project-card">
+          <img
+  src={brainDet}
+  alt="Brain Tumor Classification"
+  className="project-img"
+/>
           <h3>🧠 Brain Tumor Classification</h3>
           <p>CNN model achieving <b>92% accuracy</b> using MobileNetV2.</p>
+
+          <div className="project-gallery">
+
+  <img src={brainLogin} alt="Brain Login" />
+  <img src={tumAnly} alt="Tumor Analysis" />
+  <img src={tumDet} alt="Tumor Detection" />
+
+</div>
 
           {/* ✅ TECH STACK */}
           <div className="tech-stack">
@@ -31,8 +53,20 @@ function Projects() {
 
         {/* Project 2 */}
         <div className="project-card">
+          <img
+  src={genAiTut}
+  alt="GenAI Tutor"
+  className="project-img"
+/>
           <h3>🤖 GenAI Tutor</h3>
           <p>RAG-based assistant using embeddings and vector DB.</p>
+
+          <div className="project-gallery">
+
+  <img src={genQuiz} alt="Quiz" />
+  <img src={genImg} alt="GenAI" />
+
+</div>
 
           {/* ✅ TECH STACK */}
           <div className="tech-stack">
@@ -55,15 +89,27 @@ function Projects() {
 
         {/* Project 3 */}
         <div className="project-card">
+          <img
+  src={tripCost}
+  alt="Trip Cost Predictor"
+  className="project-img"
+/>
           <h3>✈️ Trip Cost Predictor</h3>
           <p>ML regression model for predicting travel cost.</p>
 
+          <div className="project-gallery">
+
+  <img src={tripImg} alt="Trip Predictor" />
+
+</div>
+
           {/* ✅ TECH STACK */}
           <div className="tech-stack">
-            <span>Random Forest</span>
-            <span>Flask</span>
-            <span>Regression</span>
-          </div>
+  <span>Machine Learning</span>
+  <span>Random Forest</span>
+  <span>Flask</span>
+  <span>Regression</span>
+</div>
 
           <div className="project-links">
             <a 
